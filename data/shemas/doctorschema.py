3388d4dadcfdec0fc71b83923e464ada6f73ab76
gloryacodes <gloriaoyewusii@gmail.com>
2025-04-06 11:45:41 +0100

    Endpoint to get all doctors and doctor by email and also register as a doctor committed

--- data/shemas/doctorschema.py
+++ data/shemas/doctorschema.py
@@ -6,6 +6,7 @@ from data.models.doctors import Doctors
 class DoctorSchema(Schema):
     class Meta(Schema.Meta):
         model = Doctors
+        fields = ('id', 'doctor_name', 'doctor_email', 'doctor_specialty')
 
     id = fields.String(dump_only=True)
     doctor_name = fields.String(required=True)
--- routes/doctorsroutes.py
+++ routes/doctorsroutes.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify, make_response
 from flask_mongoengine import MongoEngine
 from marshmallow import Schema, fields, post_load
 from bson import ObjectId
+
+from data.models import doctors
 from data.models.doctors import Doctors
 from data.shemas.doctorschema import DoctorSchema
 
@@ -22,9 +24,9 @@ def index():
 
 @app.route('/doctors', methods=['GET'])
 def get_doctors():
-    doctors = Doctors.objects.all()
+    all_doctors = Doctors.objects.all()
     doctor_schema = DoctorSchema(many=True)
-    doctors_data, error = doctor_schema.dump(doctors)
+    doctors_data = doctor_schema.dump(all_doctors)
     return make_response(jsonify(doctor_schema.dump(doctors_data)))
     # get_doctors = Doctors.objects.all()
     # doctor_schema = DoctorSchema(many=True)
@@ -32,15 +34,39 @@ def get_doctors():
     # return jsonify(doctor_schema.dump(doctors))
     # return make_response(jsonify({"doctors" :doctors}))
 
+@app.route('/register_doctor', methods=['POST'])
+def register_doctor():
+    doctor_data = request.get_json()
 
+    doctor_name = doctor_data.get('doctor_name')
+    doctor_password = doctor_data.get('doctor_password')
+    doctor_email = doctor_data.get('doctor_email')
+    doctor_specialty = doctor_data.get('doctor_specialty')
+
+    doctor = Doctors(
+        doctor_name=doctor_name,
+        doctor_email=doctor_email,
+        doctor_specialty=doctor_specialty,
+        doctor_password=doctor_password
+    )
+    # doctor = Doctors(doctor_name=doctor_data['doctor_name'], doctor_email=doctor_data['doctor_email'],
+    #                  doctor_password=doctor_data['doctor_password'], doctor_specialty=doctor_data['doctor_specialty'])
+
+    doctor.save()
+
+    doctor_schema = DoctorSchema()
+    doctor_data = doctor_schema.dump(doctor)
+    return make_response(jsonify({"doctor":doctor_data}), 201)
+    # return jsonify({'doctor': doctor_data})
+@app.route('/doctors/<doctor_email>', methods=['GET'])
+def get_doctor_by_email(doctor_email):
+    doctor = Doctors.objects.get_or_404(doctor_email=doctor_email)
+    doctor_schema = DoctorSchema()
+    doctor_data = doctor_schema.dump(doctor)
+    return make_response(jsonify({"doctor":doctor_data}))
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-# @app.route('/register_doctor', methods=['POST'])
-# def register_doctor():
-# #     doctor_data = request.get_json()
-#     doctor = Doctors.objects(doctor_email=doctor_data['doctor_email'], doctor_password=doctor_data['doctor_password'])
-#     doctor.save()
-#     return jsonify({'doctor': doctor_data})
+
 
